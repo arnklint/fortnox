66ac115e80e7abe98e33750e7083ffe104822fe5
Kevin Sjöberg <kev.sjoberg@gmail.com>
2011-12-07 10:49:13 +0100

    Moved specs into fortnox directory

---
+++ spec/fortnox/contact_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+module Fortnox
+  describe Customer do
+    it "is created"
+
+    it "is destroyed"
+
+    it "is updated"
+  end
+end
---
+++ spec/fortnox/invoice_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+module Fortnox
+  describe Invoice do
+    it "is created"
+
+    it "is destroyed"
+
+    it "is updated"
+  end
+end
