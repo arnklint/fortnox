3863359417e694b1235a558d5dd4d03ed100f365
Kevin Sjöberg <kev.sjoberg@gmail.com>
2011-12-07 17:45:11 +0100

    Improved invoice spec

--- spec/fortnox/invoice_spec.rb
+++ spec/fortnox/invoice_spec.rb
@@ -1,14 +1,28 @@
+# -*- encoding: utf-8 -*-
+
 require "spec_helper"
 
 module Fortnox
   describe Invoice do
-    before do
-      @valid = FactoryGirl.attributes_for(:invoice)
+    before :all do
+      Fortnox::API.establish_connection(:token => '12961a41b1fab7f93787a56074b5bc7d', :database => '66291')
     end
 
-    it "is created" do
-      invoice = Invoice.create(@valid)
+    let :attributes do
+      {        :invoice => {          :tdate      =>  future(0),          :ddate      =>  future(30),          :edate      =>  future(30),          :ourref     =>  'Kevin Sjöberg',          :yourref    =>  'Jonas Arnklint',          :roundoff   =>  1,          :freight    =>  176,          :totalvat   =>  419,          :total      =>  2095,          :contact => {            :id       =>  1,            :name     =>  'Exempel AB',            :address  =>  'Exempelgatan',            :zip      =>  '000 00',            :city     =>  'Exempel'          },          :invoicerows => [            {            :prodno =>  1,            :descr  =>  'Gitarr',            :price  =>  21,            :amount =>  50,            :vat    =>  25            },            {            :prodno =>   2,            :descr  =>   'Piano',            :price  =>   450,            :amount =>   1,            :vat    =>   25            },            {            :prodno =>   3,            :descr  =>   'Trumpet',            :price  =>   176,            :amount =>   1,            :vat    =>   25            }          ]        }
+      }
+    end
+    
+    it "should validate requried attributes" do
+      Invoice.required :name, :contact => [:name, :address]
+      invoice = Invoice.create({})
+      invoice.should raise_error(ValidationError)
+    end
+
+    it "should create invoice" do
+      invoice = Invoice.create(attributes)
       invoice.should_not raise_error
+      invoice.code.should equal(200)
     end
 
     it "is destroyed" do
