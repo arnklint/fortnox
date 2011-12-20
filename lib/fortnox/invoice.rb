2ab7e71dc549e88c5a45c3b57c6d4b486d2c127c
Kevin Sjöberg <kev.sjoberg@gmail.com>
2011-12-20 14:33:11 +0100

    Improvements in Invoice

--- lib/fortnox/invoice.rb
+++ lib/fortnox/invoice.rb
@@ -2,11 +2,22 @@ module Fortnox
   class Invoice < API
     class << self
       def create(attributes={})
-        run :post, :set_invoice, attributes
+        run :post, :set_invoice, with_root(attributes)
       end
       
-      def update(id, attributes={})
+      def update(attributes={})
+	      run :post, :set_invoice, with_root(attributes)
       end
+
+      def destroy(id)
+        run :post, :set_invoice_cancel, { :query => { :id => id } }
+      end
+      
+      private
+      
+      def with_root(attributes)
+	      attributes.merge(:root => :invoice)
+	    end
     end
   end
 end
