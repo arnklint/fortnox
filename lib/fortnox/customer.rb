2078f9079c4844419df8f43cfc8355780efbef2e
Kevin Sjöberg <kevin@Kevin-Sjobergs-MacBook-Pro.local>
2012-01-04 10:47:54 +0100

    Functionality for customer

--- lib/fortnox/customer.rb
+++ lib/fortnox/customer.rb
@@ -1,4 +1,19 @@
 module Fortnox
   class Customer < API
+    class << self
+      def create(attributes={})
+        run :post, :set_contact, with_root(attributes)
+      end
+
+      def update(attributes={})
+        run :post, :set_contact, with_root(attributes)
+      end
+
+      private
+
+      def with_root(attributes)
+        { :contact => attributes }
+      end
+    end
   end
 end
