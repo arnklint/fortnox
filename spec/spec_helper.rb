6808dadea47b72ec8e18d10985a160a5f7758028
Kevin Sjöberg <kevin@Kevin-Sjobergs-MacBook-Pro.local>
2012-01-05 01:32:18 +0100

    Configuration for VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,8 @@ def future(days)
   (Time.now + days * 86400).strftime("%Y-%m-%d")
 end
 
+VCR.config do |c|
+  c.default_cassette_options = {:match_requests_on => [:method, :uri, :body]}
+  c.cassette_library_dir = 'spec/fixtures/cassettes'
+  c.stub_with :webmock
+end
