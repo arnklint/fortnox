51867837c2b18d94df2555f7f68aa7b72e513796
Kevin Sjöberg <kevin@Kevin-Sjobergs-MacBook-Pro.local>
2012-01-05 01:30:27 +0100

    Improvements on response handling in Invoice

--- lib/fortnox/invoice.rb
+++ lib/fortnox/invoice.rb
@@ -2,15 +2,20 @@ module Fortnox
   class Invoice < API
     class << self
       def create(attributes={})
-        run :post, :set_invoice, with_root(attributes)
+        attributes = attributes.reject { |k,v| k == :id } if attributes[:id]
+
+        response = run :post, :set_invoice, with_root(attributes)
+        response['result'] ? response['result']['id'].to_i : false
       end
       
       def update(attributes={})
-	      run :post, :set_invoice, with_root(attributes)
+	      response = run :post, :set_invoice, with_root(attributes)
+        response['result'] ? response['result']['id'].to_i : false
       end
 
       def destroy(id)
-        run :post, :set_invoice_cancel, { :query => { :id => id } }
+        response = run :post, :set_invoice_cancel, { :query => { :id => id } }
+        response['result'] ? response['result'].to_i : false
       end
       
       private
