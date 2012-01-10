b0c1eccde333a64c627dc783fb23761217f04dd9
Kevin Sjöberg <kevin@Kevin-Sjobergs-MacBook-Pro.local>
2012-01-10 16:58:33 +0100

    Fix in integration spec for invoice

--- spec/integration/invoice_spec.rb
+++ spec/integration/invoice_spec.rb
@@ -4,14 +4,14 @@ module Fortnox
   describe Invoice do
     before(:each) { API.establish_connection }
 
-    it "should create and return an id" do
+    it "should create invoice and return it's id" do
       VCR.use_cassette('invoice/create') do
         invoice_no = Invoice.create(fixtures(:invoice))
         invoice_no.should be_a(Integer)
       end
     end
 
-    it "should update and return an id" do
+    it "should update invoice given by id and return it's id" do
       VCR.use_cassette('invoice/update') do
         invoice_no = Invoice.update(fixtures(:invoice))
         invoice_no.should be_a(Integer)
@@ -19,32 +19,11 @@ module Fortnox
       end
     end
 
-    it "should destroy return an id" do
+    it "should destroy invoice given by id and return it's id" do
       VCR.use_cassette('invoice/destroy') do
         invoice_no = Invoice.destroy(1)
         invoice_no.should be_a(Integer)
         invoice_no.should eq(1)
-      end
-    end
-
-    it "should fail to create and return false" do
-      VCR.use_cassette('invoice/create-fail') do
-        invoice_no = Invoice.create
-        invoice_no.should be_false
-      end
-    end
-
-    it "should fail to update and return false" do
-      VCR.use_cassette('invoice/update-fail') do
-        invoice_no = Invoice.update
-        invoice_no.should be_false
-      end
-    end
-
-    it "should fail to destroy and return false" do
-      VCR.use_cassette('invoice/destroy-fail') do
-        invoice_no = Invoice.destroy(2)
-        invoice_no.should be_false
       end
     end
   end
